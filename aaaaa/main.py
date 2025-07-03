1ad56869ce8f639cbb56616c80dc77ef7df05189
gasparswidzinski <gasparswidzinski@gmail.com>
2025-07-02 21:41:29 -0300

    empieza mejora visual con qss

--- aaaaa/main.py
+++ aaaaa/main.py
@@ -4,7 +4,11 @@ from mainwindow import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    
+    with open("estilo.qss", "r", encoding="utf-8") as f:
+        app.setStyleSheet(f.read())
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-#anal
